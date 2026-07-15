c5e53ebb15851b0db8705df0517a8ed3dc412a59
letzbug <129280428+letzbug@users.noreply.github.com>
2026-07-15 07:38:15 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window.UNIPOP_CONFIG = {
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+  downloadPasswords: {
+    linksQr: 'unipop2026',
+    modifications: 'coursmodif'
+  },
+  baseFormationUrl: 'https://www.unipop.lu/formations/',
+  maxFileSizeMb: 25
+};
