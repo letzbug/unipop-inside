cc0446558654fee843ae9760d8ddbb31af52234c
letzbug <129280428+letzbug@users.noreply.github.com>
2026-07-15 07:41:38 +0200

    Update Supabase configuration with new URL and key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.UNIPOP_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://hhxpsawlyfymlrlukaty.supabase.co/rest/v1/',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoeHBzYXdseWZ5bWxybHVrYXR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNzA0ODMsImV4cCI6MjA5OTY0NjQ4M30.YN8T8E4RD1wcDexpNbG8s1W3bSmBjGOpyCWXLyFdw5s',
   downloadPasswords: {
     linksQr: 'unipop2026',
     modifications: 'coursmodif'
