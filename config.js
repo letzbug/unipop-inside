353caa9d70f0a886b65fa328916fb544e8a2baae
letzbug <129280428+letzbug@users.noreply.github.com>
2026-07-15 07:53:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
 window.UNIPOP_CONFIG = {
-  supabaseUrl: 'https://hhxpsawlyfymlrlukaty.supabase.co/rest/v1/',
+  supabaseUrl: 'https://hhxpsawlyfymlrlukaty.supabase.co',
+
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoeHBzYXdseWZ5bWxybHVrYXR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNzA0ODMsImV4cCI6MjA5OTY0NjQ4M30.YN8T8E4RD1wcDexpNbG8s1W3bSmBjGOpyCWXLyFdw5s',
+
+  storageBucket: 'unipop-files',
+
   downloadPasswords: {
     linksQr: 'unipop2026',
     modifications: 'coursmodif'
   },
+
   baseFormationUrl: 'https://www.unipop.lu/formations/',
-  maxFileSizeMb: 25
+
+  maxFileSizeMb: 25,
+
+  appName: 'UniPop Inside',
+
+  schoolYearDefault: '2026/2027'
 };
